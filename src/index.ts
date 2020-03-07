473ab92ace16fa28d1310d3628b9d236b60a1f09
carlosperate <carlosperate@users.noreply.github.com>
2020-03-07 19:03:38 +0000

    Action: Start using the download lib to get the file.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,28 @@
 import * as core from '@actions/core';
+const download = require('download');
 
-try {
-  // Get Action inputs
-  const fileURL = core.getInput('file-url');
-  const fileName = core.getInput('file-name');
-  const fileLocation = core.getInput('location');
-  core.info(`url: ${fileURL}`);
-  core.info(`name: ${fileName}`);
-  core.info(`location: ${fileLocation}`);
-  core.setOutput('file-path', `${fileLocation}/${fileName}`);
-} catch (error) {
-  core.setFailed(error.message);
+async function main(): Promise<void> {
+  try {
+    const fileURL: string = core.getInput('file-url');
+    const fileName: string | undefined = core.getInput('file-name') || undefined;
+    const fileLocation: string = core.getInput('location') || process.cwd();
+
+    if (!fileURL) {
+      core.warning('the file-url input was not set.');
+    }
+
+    core.info(`url: ${fileURL}`);
+    core.info(`name: ${fileName}`);
+    core.info(`location: ${fileLocation}`);
+    await download(fileURL, fileLocation, {
+      extract: false,
+      filename: fileName,
+    });
+
+    core.setOutput('file-path', `${fileLocation}/${fileName}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+main();
